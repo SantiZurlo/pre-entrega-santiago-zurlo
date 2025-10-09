9273d1ea47ccf8b2b974110d84007bbcca74f050
Santiago <santizurlo05@gmail.com>
2025-10-09 18:39:12 -0300

    GET products

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola");
+
+
+let [, , method, resource] = process.argv;
+
+method = method.toUpperCase();
+resource = resource.toLowerCase();
+
+if(method == "GET" && resource == "products"){
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
