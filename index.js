155e0f40a790390208bbea1c7598247e0bf63280
Santiago <santizurlo05@gmail.com>
2025-10-09 18:53:29 -0300

    GET products ID

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 let [, , method, resource] = process.argv;
 
 method = method.toUpperCase();
@@ -9,4 +7,18 @@ if(method == "GET" && resource == "products"){
 fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
   .then(data => console.log(data));
+}
+
+if(method == 'GET' && resource.startsWith('products/')){
+let id = resource.split("/")[1];
+id = parseInt(id);
+
+if(isNaN(id) || id <= 0){
+    console.log("No valido");
+}
+
+fetch('https://fakestoreapi.com/products/' + id)
+  .then(response => response.json())
+  .then(data => console.log(data));
+
 }
